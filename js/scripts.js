a0262dfd501133965a3051a1e62b8c0f38723b6a
Yesenya <perez.yesenya@gmail.com>
2018-02-03 13:19:23 -0800

    add workshop project to git

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("#goal-button").click(function() {
+$("#goal-list").append("<li class='list-item'>"+ $("#new-goal").val() + "</li>");
+  $("#new-goal").val("");
+
+
+  })
+})
